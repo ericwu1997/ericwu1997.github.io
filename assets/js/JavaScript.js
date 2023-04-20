ba69514b0bd6ccff890b2993c5ede41c6897100a
Guo Cheng <zhonghe.quanzhong@gmail.com>
2023-04-19 22:32:55 -0700

    a

--- assets/js/JavaScript.js
+++ assets/js/JavaScript.js
@@ -5,6 +5,34 @@ document.addEventListener("scroll", (event) => {
     document.getElementById('hamburger-menu').classList.remove('open')
 });
 
+function scrollToTop() {
+    scrollToSmoothly(document.getElementById('header').offsetTop, 200)
+}
+
+function scrollToBottom() {
+    scrollToSmoothly(document.getElementById('footer').offsetTop, 200)
+}
+
+function scrollToSmoothly(pos, time) {
+    var currentPos = window.pageYOffset;
+    var start = null;
+    if(time == null) time = 500;
+    pos = +pos, time = +time;
+    window.requestAnimationFrame(function step(currentTime) {
+        start = !start ? currentTime : start;
+        var progress = currentTime - start;
+        if (currentPos < pos) {
+            window.scrollTo(0, ((pos - currentPos) * progress / time) + currentPos);
+        } else {
+            window.scrollTo(0, currentPos - ((currentPos - pos) * progress / time));
+        }
+        if (progress < time) {
+            window.requestAnimationFrame(step);
+        } else {
+            window.scrollTo(0, pos);
+        }
+    });
+}
 
 function renderGalleryView(list) {
     list.sort((a, b) => {
